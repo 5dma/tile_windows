dffe9f9f76dcc1844f26d659ced80a140ed7216d
5dma <mark@lautman.net>
2024-12-23 21:55:22 -0500

    Identify open windows.

--- headers/headers.h
+++ headers/headers.h
@@ -1,12 +1,2 @@
-#include <regex.h>
 
-#define LONGEST_STRING 1024
-typedef struct  {
-	char title[1024];
-	unsigned short int current_length;
-	unsigned short int current_width;
-	struct WindowStruct *next;
-} WindowStruct;
 
-regex_t *make_window_regex();
-regex_t *make_monitor_regex();
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <headers.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,8 +16,30 @@ int main(int argc, char *argv[]) {
 
 	int screen = XDefaultScreen(display);
 	int display_height = DisplayHeight(display, screen); /* Display height/width still needed for hiding clients */
-    int display_width = DisplayWidth(display, screen);
+	int display_width = DisplayWidth(display, screen);
+	int screen_count = XScreenCount( display );
 
+	Window root = DefaultRootWindow(display);
+	Window root_return;
+	Window parent;
+	Window *children = NULL;
+	unsigned int nchildren;
+	Status status = XQueryTree(display, root, &root_return, &parent, &children, &nchildren);
+	XWindowAttributes window_attributes;
+	XTextProperty text_property;
+	int visible = 0;
+	printf("class depth border_width map_state\n");
+	for (int i = 0; i < nchildren; i++) {
+		Status status = XGetWindowAttributes(display, children[i], &window_attributes);
+		printf("%d %d %d %d\n", window_attributes.class, window_attributes.depth, window_attributes.border_width, window_attributes.map_state);
+		if ((window_attributes.map_state == IsViewable) && (window_attributes.class == InputOutput)) {
+		//if ((window_attributes.class == 1) && (window_attributes.map_state == 2))
+			Status inside_status = XGetWMName(display, children[i], &text_property);
+			visible++;
+		}
+	}
+	printf("Number of visible windows %d\n", visible);
+	XFree(children);
 	XCloseDisplay(display);
 	return 0;
 }
