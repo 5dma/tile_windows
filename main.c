02901e4e86685802591f63f50b20c62c0ba173b2
5dma <mark@lautman.net>
2025-03-04 11:22:20 -0500

    Call XQueryTree

--- main.c
+++ main.c
@@ -1,45 +1,46 @@
-#include <headers.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <stdio.h>
+#include <X11/Xatom.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-#define NUMBER_MATCHES 5
+#define MAX_PROPERTY_VALUE_LEN 4096
 
 int main(int argc, char *argv[]) {
-	printf("hello\n");
 	Display * display = XOpenDisplay(NULL);
 	if (display == NULL) {
 		printf("Could not connect to the display server. Exiting.\n");
 		return -1;
 	}
-
-	int screen = XDefaultScreen(display);
-	int display_height = DisplayHeight(display, screen); /* Display height/width still needed for hiding clients */
-	int display_width = DisplayWidth(display, screen);
-	int screen_count = XScreenCount( display );
-
 	Window root = DefaultRootWindow(display);
-	Window root_return;
-	Window parent;
-	Window *children = NULL;
-	unsigned int nchildren;
-	Status status = XQueryTree(display, root, &root_return, &parent, &children, &nchildren);
-	XWindowAttributes window_attributes;
-	XTextProperty text_property;
-	int visible = 0;
-	printf("class depth border_width map_state\n");
-	for (int i = 0; i < nchildren; i++) {
-		Status status = XGetWindowAttributes(display, children[i], &window_attributes);
-		printf("%d %d %d %d\n", window_attributes.class, window_attributes.depth, window_attributes.border_width, window_attributes.map_state);
-		if ((window_attributes.map_state == IsViewable) && (window_attributes.class == InputOutput)) {
-		//if ((window_attributes.class == 1) && (window_attributes.map_state == 2))
-			Status inside_status = XGetWMName(display, children[i], &text_property);
-			visible++;
-		}
+	unsigned int size;
+	Atom xa_ret_type;
+	int ret_format;
+	unsigned long ret_nitems;
+	unsigned long ret_bytes_after;
+	unsigned char *ret_prop;
+	
+	int number_properties;
+	Atom * property_list = XListProperties(display, root, &number_properties);
+	for (int i=0; i<number_properties; i++) {
+		printf("Property %d %ld\n", i, property_list[i]);
+		
 	}
-	printf("Number of visible windows %d\n", visible);
-	XFree(children);
+/* 
+	int property = XGetWindowProperty(display, root,"_NET_CLIENT_LIST", 0, 4096, False,
+            XA_WINDOW, &xa_ret_type, &ret_format,     
+            &ret_nitems, &ret_bytes_after, &ret_prop); */
+
+	Window root_return;
+	Window parent_return;
+	Window *children_return;
+	unsigned int nchildren_return;
+	Status status = XQueryTree(display, root, &root_return, &parent_return, &children_return, &nchildren_return);
+	if (status == 0) {
+		printf("Fatal error. XQueryTree failed with a status %d\n", status);
+	}
 	XCloseDisplay(display);
 	return 0;
 }
