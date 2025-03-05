c2b038528897285eddf713059fc45c43c9ed1cff
5dma <mark@lautman.net>
2025-03-05 15:01:19 -0500

    Get detailed properties of each window.

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 	Window root = DefaultRootWindow(display);
-	unsigned int size;
+	/*unsigned int size;
 	Atom xa_ret_type;
 	int ret_format;
 	unsigned long ret_nitems;
@@ -30,12 +30,14 @@ int main(int argc, char *argv[]) {
 		strcpy(property_name, XGetAtomName(display, property_list[i]));
 		printf("Property %d %ld %s\n", i, property_list[i], property_name);
 		
-	}
+	}*/
 /* 
 	int property = XGetWindowProperty(display, root,"_NET_CLIENT_LIST", 0, 4096, False,
             XA_WINDOW, &xa_ret_type, &ret_format,     
             &ret_nitems, &ret_bytes_after, &ret_prop); */
 
+	int number_properties;
+	char property_name[MAX_PROPERTY_NAME];
 	Window root_return;
 	Window parent_return;
 	Window *children_return;
@@ -51,9 +53,11 @@ int main(int argc, char *argv[]) {
 	outfile = fopen("/tmp/stuff.csv","w");
 	FILE *property_file = fopen("/tmp/property_file.csv","w");
 	fprintf(outfile, "window\tx\ty\twidth\theight\tborder_width\tdepth\troot\tbit_gravity\twin_gravity\tmap_state\tnumber_properties\n");
+	fprintf(property_file, "window\tAtom\tName\tactual_type\tactual_format\tnitems_return\tbytes_after_return\tprop_return\n");
+
 	for (unsigned int i = 0; i<nchildren_return; i++) {
 		current_window = children_return[i];
-		Atom * property_list = XListProperties(display, current_window, &number_properties);
+		Atom *property_list = XListProperties(display, children_return[i], &number_properties);
 		status = XGetWindowAttributes(display, current_window, &x_window_attributes);
 		if (status != 0) {
 		fprintf(outfile, "%ld\t%d\t%d\t%d\t%d\t%d\t%d\t%ld\t%d\t%d\t%d\t%d\n",
@@ -71,9 +75,31 @@ int main(int argc, char *argv[]) {
 				number_properties);
 				
 				Atom * property_list = XListProperties(display, current_window, &number_properties);
+				Atom actual_type_return;
+				int actual_format_return;
+				unsigned long nitems_return;
+				unsigned long bytes_after_return;
+				unsigned char *prop_return;
+				int get_property;
 				for (int i=0; i<number_properties; i++) {
 					strcpy(property_name, XGetAtomName(display, property_list[i]));
-					fprintf(property_file, "%ld\t%ld\t%s\n", current_window, property_list[i], property_name);
+
+					get_property = XGetWindowProperty( display, current_window, property_list[i], 0, 32, False, AnyPropertyType, &actual_type_return, &actual_format_return, &nitems_return, &bytes_after_return, &prop_return);
+					if (get_property == Success) {
+						//printf("%s\n", prop_return);
+						fprintf(property_file, "%ld\t%ld\t%s\t%ld\t%d\t%ld\t%ld\t%s\n",
+							current_window, 
+							property_list[i], 
+							property_name,
+							actual_type_return,
+							actual_format_return,
+							nitems_return,
+							bytes_after_return,
+							prop_return);
+
+					} else {
+						printf("Failed on getting a property\n");
+					}
 				}
 		} else {
 			printf("Got a status error on XGetWindowAttributes\n");
