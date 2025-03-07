0d865c0daf95a1e2acc96378e5529d14b3519c55
5dma <mark@lautman.net>
2025-03-06 20:35:33 -0500

    Reformat code.

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #define SPACING 20 /* Spacing between each window's top-left corner. */
 
 /* A record containing pertinent information about a visible window. */
-typedef struct Window_Info{
+typedef struct Window_Info {
 	Window window;
 	int x;
 	int y;
@@ -27,9 +27,8 @@ typedef struct Layout_Info {
 	Display *display; /* XWindow display. */
 } Layout_Info;
 
-
-void move_resize_windows(gpointer data,  gpointer user_data) {
-	Layout_Info *layout_info = (Layout_Info *) user_data;
+void move_resize_windows(gpointer data, gpointer user_data) {
+	Layout_Info *layout_info = (Layout_Info *)user_data;
 	Window_Info *window_info = (Window_Info *)data;
 	layout_info->current_window_position++;
 	/* Don't move the background or the panel. */
@@ -39,15 +38,14 @@ void move_resize_windows(gpointer data,  gpointer user_data) {
 	if (window_info == layout_info->panel) {
 		return;
 	}
-	
+
 	unsigned int spacing = layout_info->current_window_position * SPACING;
-	XMoveWindow(layout_info->display, window_info->window,spacing, spacing);
+	XMoveWindow(layout_info->display, window_info->window, spacing, spacing);
 	/* If the moved window is wider than the background width, make it narrower. */
 	if (window_info->width + (spacing) > layout_info->background->width) {
-		XResizeWindow(layout_info->display, window_info->window,  layout_info->background->width - spacing, window_info->height);
+		XResizeWindow(layout_info->display, window_info->window, layout_info->background->width - spacing, window_info->height);
 	}
 }
-
 
 int main(int argc, char *argv[]) {
 	/* Passed as user data to move_resize_windows(). */
@@ -60,17 +58,17 @@ int main(int argc, char *argv[]) {
 		printf("Could not connect to the display server. Exiting.\n");
 		return -1;
 	}
-	
+
 	/* Get the root window. */
 	Window root = DefaultRootWindow(layout_info.display);
-	
+
 	/* Set up variables passed to XQueryTree(). */
 	Window root_return;
 	Window parent_return;
 	Window *children_return;
 	Status status;
 	unsigned int nchildren_return;
-	
+
 	status = XQueryTree(layout_info.display, root, &root_return, &parent_return, &children_return, &nchildren_return);
 	if (status == 0) {
 		printf("Fatal error. XQueryTree failed with a status %d. Exiting.\n", status);
@@ -84,14 +82,13 @@ int main(int argc, char *argv[]) {
 
 	/* Loop over all windows discovered by XQueryTree, which is typically quite a bit more than the visible windows. */
 	for (unsigned int i = 0; i < nchildren_return; i++) {
-		status = XGetWindowAttributes(layout_info.display , children_return[i], &x_window_attributes);
+		status = XGetWindowAttributes(layout_info.display, children_return[i], &x_window_attributes);
 		if (status != 0) {
-			
-			/* Filter for only those windows that are visible. See 
+			/* Filter for only those windows that are visible. See
 				https://stackoverflow.com/questions/78701376/xquerytree-returns-wrong-number-of-child-windows */
 			if ((x_window_attributes.map_state == IsViewable) && (x_window_attributes.override_redirect == 0)) {
 				/* Create a new Window_Info for this visible window, and add it to the GS List. */
-				Window_Info *window_info = (Window_Info *) g_malloc(sizeof(Window_Info));
+				Window_Info *window_info = (Window_Info *)g_malloc(sizeof(Window_Info));
 				window_info->window = children_return[i];
 				window_info->x = x_window_attributes.x;
 				window_info->y = x_window_attributes.y;
@@ -106,19 +103,19 @@ int main(int argc, char *argv[]) {
 
 	/* Save information about the first window in the list (Background) and the
 	last window in the list (panel). */
-	gpointer trash = g_slist_nth_data (visible_windows, 0);
-	Window_Info *background_info = (Window_Info *) trash;
+	gpointer trash = g_slist_nth_data(visible_windows, 0);
+	Window_Info *background_info = (Window_Info *)trash;
 	/* Using g_slist_last() here doesn't work; why not? */
 	unsigned int list_length = g_slist_length(visible_windows);
-	gpointer trash2 = g_slist_nth_data (visible_windows, list_length - 1);
-	Window_Info *panel_info = (Window_Info *) trash2;
+	gpointer trash2 = g_slist_nth_data(visible_windows, list_length - 1);
+	Window_Info *panel_info = (Window_Info *)trash2;
 	layout_info.background = background_info;
 	layout_info.panel = panel_info;
 	layout_info.height = background_info->height - panel_info->height;
 
 	/* Go move the windows.*/
-	g_slist_foreach (visible_windows, move_resize_windows, &layout_info);
+	g_slist_foreach(visible_windows, move_resize_windows, &layout_info);
 
-	XCloseDisplay(layout_info.display );
+	XCloseDisplay(layout_info.display);
 	return 0;
 }
